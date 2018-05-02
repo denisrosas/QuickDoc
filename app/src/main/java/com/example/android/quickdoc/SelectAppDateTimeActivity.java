ff3fc4de483946200c667ce27e1f3217170f2b86
Denis <denis.rosas@gmail.com>
2018-05-01 21:25:51 -0400

    Created 2 new activities that will be necessary in the future. Changed the DoctorDailyAppointments class to set the 16 positions to "" instead of null.

---
+++ app/src/main/java/com/example/android/quickdoc/SelectAppDateTimeActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.quickdoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectAppDateTimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_app_date_time);
+    }
+}
--- app/src/main/java/com/example/android/quickdoc/dataClasses/DoctorDailyAppointments.java
+++ app/src/main/java/com/example/android/quickdoc/dataClasses/DoctorDailyAppointments.java
@@ -7,7 +7,14 @@ public class DoctorDailyAppointments {
 
     private static final int APPOINTMENTS_PER_DAY = 16;
 
-    //for every position in the daily agenda, if the String is null, means it's not reserved
-    public ArrayList<String> reservationArray = new ArrayList<>();
+    /** for every position in the daily agenda, if the String is null, means it's not reserved */
+    public ArrayList<String> reservationArray;
 
+    public DoctorDailyAppointments(){
+        this.reservationArray =  new ArrayList<>();
+
+        for(int index=0; index<APPOINTMENTS_PER_DAY; index++){
+            this.reservationArray.add(index, "");
+        }
+    }
 }
